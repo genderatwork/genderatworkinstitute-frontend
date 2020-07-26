1c60bb2658edf655147812de43a5a1864761d5e7
Raj Raj <raj@cambridgespark.com>
2020-07-26 11:11:32 +0100

    Added alternating course rows

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-function initPage() {
+function alternateOrderOfCourses() {
+  $('article.course a').each(function(index) {
+    $(this).addClass(index % 2 == 0 ? 'even' : 'odd');
+  });
+}
+
+function initSearch() {
   var navlist = $('.container-navbar-links ul.navbar-links')[1]
-  console.log(navlist);
   var navsearch = $('#navbar-search-form')[0]
-  console.log(navsearch);
-  // navlist.insertBefore(navsearch, navlist.firstChild)
   navlist.appendChild(navsearch);
   navsearch.style.display = 'inline-block'
+}
+
+function initPage() {
+  initSearch();
+  alternateOrderOfCourses();
 }
 
 $(document).ready(initPage);
